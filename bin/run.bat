2c3f4f027c96aaef126d123b34c1f7027696426c
duongnh <duongnh.nava@gmail.com>
2018-05-11 14:11:44 +0700

    test app

---
+++ bin/run.bat
@@ -0,0 +1,3 @@
+Title=AHP-SERVICE [2] BRAND - VAS - VIVAS - VMG- [PORT:9982]
+set path=C:\Program Files\Java\jdk1.8.0_141\bin
+java -Xmx512M -classpath "../lib/bootstrap.jar" com.gk.htc.ahp.brand.bootstrap.Bootstrap
---
+++ bin/run.sh
@@ -0,0 +1,1 @@
+java -Xmx1024M -classpath "../lib/Brand_HTC-3.1-bootstrap.jar" com.gk.htc.ahp.brand.bootstrap.Bootstrap
